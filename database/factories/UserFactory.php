8953ba441e2488d9b3b458968325360499625c2c
noyamii <batebinim@gmail.com>
2024-11-25 18:25:45 +0330

    first

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class UserFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'phone' => fake()->unique()->phoneNumber(),
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
+            'mac_address' => Str::random(10),
+            'email' => fake()->unique()->safeEmail(),
+            'email_verified_at' => now(),
+        ];
+    }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     */
+    public function unverified(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'email_verified_at' => null,
+        ]);
+    }
+}
---
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->string('phone')->primary();
+            $table->string('first_name', 25);
+            $table->string('last_name', 25);
+            
+            // reason: only one device allowed
+            $table->string('mac_address');
+
+            $table->string('email')->unique()->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('update_at')->useCurrentOnUpdate();
+        });
+
+        Schema::create('otp_code', function (Blueprint $table) {
+            $table->string('phone')->unique();
+            $table->string('code', 6);
+
+            // delete after certain time
+            $table->timestamp('created_at')->useCurrent();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('otp_code');
+    }
+};
